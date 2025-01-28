567aee39cfb6a23ab17cd8247c73d226a697b0fa
kimgyejeong <pmwkdo@gmail.com>
2025-01-28 21:07:40 +0900

    client/src/App.js - react와 express 코드 통신 확인 server/src/app.js - react와 통신하기 위해 cors 추가.

--- server/src/app/app.js
+++ server/src/app/app.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 const {UserRoutes} = require("./routes/userRoutes");
 
 class App {
@@ -11,10 +13,24 @@ class App {
     setMiddlewares() {
         this.app.use(express.json());
         this.app.use(express.urlencoded({extended: true}));
+        // CORS 설정 (React 서버 URL을 명시)
+        this.app.use(cors({
+            origin: "http://localhost:3000", // React 개발 서버 주소
+            methods: ["GET", "POST", "PUT", "DELETE"], // 허용할 HTTP 메서드
+            credentials: true // 쿠키 인증이 필요한 경우 설정
+        }));
     }
 
     setRoutes() {
         const userRoutes = new UserRoutes();
+        //test code. react
+        this.app.get('/api/test', (req, res) => {
+            console.log('GET /api/test');
+            res.send(JSON.stringify({
+                "test":"OK",
+            }))
+        } );
+        //End test code. react
         this.app.use('/api/users', userRoutes.router);
     }
 
