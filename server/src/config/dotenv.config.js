e8482d613b55563a48dd9ace4c284954f92d16aa
eunji <helena712@gmail.com>
2025-02-17 22:26:42 +0900

    dotenv > /env

--- server/src/config/dotenv.config.js
+++ server/src/config/dotenv.config.js
@@ -8,12 +8,11 @@ class DotenvConfig {
     static load() {
         // const envPath = path.resolve(process.cwd(), '.env');
         const envPathForMac = path.resolve(__dirname, '../../.env');
-        const envPathForWindow = path.resolve(__dirname, '../../.env');
+        const envPathForWindow = path.resolve(__dirname, '../../env');
         const envPath = fs.existsSync(envPathForMac) ? envPathForMac : envPathForWindow;
         const result = dotenv.config({ path: envPath });
 
         console.log('dot env path : ', envPath);
-
         if (result.error) {
             console.error('환경 변수 파일(.env)을 로드하는 데 실패했습니다:', result.error);
             process.exit(1); // 실패 시 프로세스 종료
